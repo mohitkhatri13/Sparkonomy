d7c53fc7c4be327638c10b3a012f48b0a088e758
mohit khatri <khatrimohit2002@gmail.com>
2025-09-16 03:50:30 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,20 @@ import Navbar from "./Components/Navbar";
 
 function App() {
   const containerRef = useRef(null);
-useEffect(() => {
-  gsap.fromTo(
-    containerRef.current,
-    { y: 150, opacity: 0 }, 
-    { 
-      y: 0, 
-      opacity: 1, 
-      duration: 2,     
-      ease: "power2.out", 
-      delay: 0.3       
-  );
-}, []);
 
+  useEffect(() => {
+    gsap.fromTo(
+      containerRef.current,
+      { y: 150, opacity: 0 },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 2,
+        ease: "power2.out",
+        delay: 0.3,
+      }
+    );
+  }, []);
 
   return (
     <div className="bg-pink-100 h-full w-full flex justify-center">

d57b1abacb61c5f45b1b5fb0b1b4e041824b6a30
mohit khatri <khatrimohit2002@gmail.com>
2025-09-16 03:44:10 +0530

    animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
-import './App.css'
-import Main from './Components/Main'
-import Navbar from './Components/Navbar'
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import "./App.css";
+import Main from "./Components/Main";
+import Navbar from "./Components/Navbar";
+
 function App() {
-  
+  const containerRef = useRef(null);
+useEffect(() => {
+  gsap.fromTo(
+    containerRef.current,
+    { y: 150, opacity: 0 }, 
+    { 
+      y: 0, 
+      opacity: 1, 
+      duration: 2,     
+      ease: "power2.out", 
+      delay: 0.3       
+  );
+}, []);
+
 
   return (
-    <div className='bg-pink-100  h-full w-full  flex  justify-center'>
-    <div className='   w-[390px] mt-3   flex flex-col items-center justify-center'>
-     <Navbar/>
-     <Main/>
-      
+    <div className="bg-pink-100 h-full w-full flex justify-center">
+      <div
+        ref={containerRef}
+        className="w-[390px] mt-3 flex flex-col items-center justify-center"
+      >
+        <Navbar />
+        <Main />
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
